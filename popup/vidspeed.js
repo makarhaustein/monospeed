7554fc7f30d1a2943b2d18b7f23cd1b4fb93bfbb
Makar Haustein <makarmaxha@gmail.com>
2024-11-28 16:46:41 +0100

    Speed now displayed

--- popup/vidspeed.js
+++ popup/vidspeed.js
@@ -20,7 +20,7 @@ function storageAccessed(stored) {
 
     if (!stored.speed) {
         browser.storage.local.set({ speed: 1 });
-        speed = 1;
+        speed = 1.0;
     } else {
         speed = stored.speed;
     }
@@ -29,12 +29,13 @@ function storageAccessed(stored) {
         if (speed <= 0.1) {
             speed = 0.1;
         }
-        if (speed >= 8) {
-            speed = 8;
+        if (speed >= 8.0) {
+            speed = 8.0;
         }
         for (const tab of tabs) {
             browser.tabs.sendMessage(tab.id, { speed: speed });
         }
+        speedField.innerHTML = speed.toFixed(1);
         browser.storage.local.set({ speed: speed });
     }
 
@@ -43,7 +44,7 @@ function storageAccessed(stored) {
     }
 
     speedField.onclick = () => {
-        speed = 1;
+        speed = 1.0;
         getTabs().then(updateSpeedCheck, onError);
     };
 
